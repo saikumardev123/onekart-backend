d5cc265811b84c650f4c835e9a773830ca9236c8
sai <sai@digital-lync.com>
2021-06-02 16:02:21 +0530

    2june2021

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,9 +5,9 @@ const userModel = require('../models/user.model');
 
 exports.register = async (req, res) => {
 
-    var user =  {
+    var user = {
         username: req.body.username,
-        password: bcrypt.hashSync(req.body.password,10),
+        password: bcrypt.hashSync(req.body.password, 10),
         email: req.body.email,
     };
 
@@ -24,3 +24,61 @@ exports.register = async (req, res) => {
         return res.status(500).send({ success: false, message: error.message })
     }
 }
+
+exports.login = async (req, res) => {
+
+    try {
+        let user = await userModel.findOne({ username: req.body.username });
+        if (user) {
+            if (bcrypt.compareSync(req.body.password, user.password)) {
+                res.status(200).send({ success: true, message: "Login success!" });
+            }
+            else {
+                res.status(401).send({ success: true, message: "Password incorrect" });
+            }
+        }
+
+        else
+            res.status(404).send({ success: false, message: "user not found" });
+
+    } catch (error) {
+        return res.status(500).send({ success: false, message: error.message })
+    }
+}
+
+exports.changePassword = async (req, res) => {
+    try {
+        let user = await userModel.findOne({ username: req.body.username });
+        if (user) {
+            if (bcrypt.compareSync(req.body.currentPassword, user.password)) {
+                let updatedUser = await userModel.findOneAndUpdate({ username: req.body.username }, { password: bcrypt.hashSync(req.body.newPassword, 10) },{new:true});
+                if (updatedUser) {
+                    if (bcrypt.compareSync(req.body.newPassword, updatedUser.password)) {
+                        res.status(200).send({ success: true, message: "password changed!" });
+                    }
+                    else {
+                        res.status(500).send({ success: false, message: "Something went wrong!!" });
+                    }
+                }
+                else {
+                    res.status(401).send({ success: false, message: "failed in updating password " });
+                }
+            }
+            else {
+                res.status(401).send({ success: true, message: "Current Password incorrect" });
+            }
+        }
+
+        else
+            res.status(404).send({ success: false, message: "user not found" });
+
+    } catch (error) {
+        console.log("inside catch block")
+        console.log(error);
+        return res.status(500).send({ success: false, message: error.message })
+    }
+}
+
+//$2b$10$Xmn5xA552FjOwiTVIwxRT.jFAUCzi4Qzr5hZdZnxsKPNcUqjc8lRy
+
+// $2b$10$Xmn5xA552FjOwiTVIwxRT.jFAUCzi4Qzr5hZdZnxsKPNcUqjc8lRy
--- routes/user.route.js
+++ routes/user.route.js
@@ -6,4 +6,8 @@ const userController = require('../controllers/user.controller');
 
 userRouter.post('/register',userController.register);
 
+userRouter.post('/login', userController.login);
+
+userRouter.put('/changePassword', userController.changePassword);
+
 module.exports = userRouter;
